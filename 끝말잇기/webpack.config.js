301c405149fea103727596159669ff745d110249
seeker1207 <jm3420@naver.com>
2021-09-22 23:02:48 +0900

    핫리로딩 기능 실습

--- 끝말잇기/webpack.config.js
+++ 끝말잇기/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const RefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
 module.exports = {
     name: 'word-relay-setting', // 이 세팅이 어떤 세팅인지
@@ -18,15 +19,33 @@ module.exports = {
                 test: /\.jsx?/,
                 loader: 'babel-loader',
                 options: {
-                    presets: ['@babel/preset-env', '@babel/preset-react']
+                    presets: [['@babel/preset-env', {
+                        targets: 'ie 11',
+                        debug: true,
+                    }], '@babel/preset-react'],
+                    plugins: [
+                        'react-refresh/babel'
+                    ]
                 }
             }
         ]
     },
-
+    plugins: [
+        new RefreshWebpackPlugin()
+    ]
+    ,
     output: {
         path: path.join(__dirname, 'dist'), // 현재폴더(__dirname)에 dist 폴더 추가
         filename: 'app.js',
+        publicPath: '/dist'
     }, // 출력
+
+    devServer: {
+        devMiddleware: { publicPath: '/dist' },
+        static: { directory: path.resolve(__dirname) },
+        hot: true
+    },
+
+    target: ['web', 'es5'],
 
 };
