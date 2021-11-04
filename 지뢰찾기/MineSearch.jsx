b75606cf9a6fc84c0f212d59930431a84bae1f98
seeker1207 <jm3420@naver.com>
2021-11-04 17:22:41 +0900

    지뢰찾기 테이블 동적 생성 및 지뢰 랜덤으로 배치하기

--- 지뢰찾기/MineSearch.jsx
+++ 지뢰찾기/MineSearch.jsx
@@ -23,6 +23,33 @@ const initialState = {
     result: '',
 }
 
+const plantMine = (row, cell, mine) => {
+    console.log(row, cell, mine);
+    const candidate = Array(row * cell).fill().map((arr, i) => {
+        return i;
+    });
+    const shuffle = [];
+    while (candidate.length > row * cell - mine) {
+        const chosen = candidate.splice(Math.floor(Math.random() * candidate.length), 1)[0];
+        shuffle.push(chosen);
+    }
+    const data = [];
+    for (let i = 0; i < row; i++) {
+        const rowData = [];
+        data.push(rowData);
+        for (let j = 0; j < cell; j++) {
+            rowData.push(CODE.NORMAL);
+        }
+    }
+
+    for (let k = 0; k < shuffle.length; k++) {
+        const ver = Math.floor(shuffle[k] / cell);
+        const hor = shuffle[k] % cell;
+        data[ver][hor] = CODE.MINE;
+    }
+    console.table(data);
+    return data;
+}
 export const START_GAME = 'START_GAME';
 
 const reducer = (state, action) => {
@@ -38,7 +65,7 @@ const reducer = (state, action) => {
 }
 const MineSearch = () => {
     const [state, dispatch] = useReducer(reducer, initialState);
-    const value = useMemo( () => ({ tableData: state.tableData, dispatch }, [state.tableData]));
+    const value = useMemo( () => ({ tableData: state.tableData, dispatch }), [state.tableData]);
 
     return (
         <TableContext.Provider value={value}>
--- 지뢰찾기/Table.jsx
+++ 지뢰찾기/Table.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import Tr from "./Tr";
+import {TableContext} from "./MineSearch";
 
 const Table = () => {
+    const { tableData } = useContext(TableContext);
     return (
-        <div>
-            
-        </div>
+        <table>
+            {Array(tableData.length).fill().map((tr, i) => <Tr rowIndex={i}/>)}
+        </table>
     );
 };
 
