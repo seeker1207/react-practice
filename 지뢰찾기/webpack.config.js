66e0136c84977d1df8490d2d22886afa2e029366
seeker1207 <jm3420@naver.com>
2021-11-04 16:12:43 +0900

    지뢰찾기 프로젝트 생성 및 리듀서, useMemo 실습

---
+++ 지뢰찾기/webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const RefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+
+module.exports = {
+    name: 'bomb-pang-setting', // 이 세팅이 어떤 세팅인지
+    mode: 'development', // 실서비스에서는 production
+    devtool: 'eval',
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+
+    entry: {
+        app: ['./client'],
+    },  //임력
+
+    module: {
+        rules: [
+            {
+                test: /\.jsx?/,
+                loader: 'babel-loader',
+                options: {
+                    presets: [['@babel/preset-env', {
+                        targets: 'ie 11',
+                        debug: true,
+                    }], '@babel/preset-react'],
+                    plugins: [
+                        'react-refresh/babel'
+                    ]
+                }
+            }
+        ]
+    },
+    plugins: [
+        new RefreshWebpackPlugin()
+    ]
+    ,
+    output: {
+        path: path.join(__dirname, 'dist'), // 현재폴더(__dirname)에 dist 폴더 추가
+        filename: 'app.js',
+        publicPath: '/dist'
+    }, // 출력
+
+    devServer: {
+        devMiddleware: { publicPath: '/dist' },
+        static: { directory: path.resolve(__dirname) },
+        hot: true
+    },
+
+    target: ['web', 'es5'],
+
+};
